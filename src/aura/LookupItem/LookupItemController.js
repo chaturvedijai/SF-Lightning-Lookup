b372f5ae37fb2ac7dc4dbad3dfdcf91cbbc02fd2
Ravi Narayanan <ravnarayanan@paypal.com>
2017-12-29 16:31:52 +0530

    Adding Relationship fields in subHeading Logic

--- src/aura/LookupItem/LookupItemController.js
+++ src/aura/LookupItem/LookupItemController.js
@@ -6,8 +6,17 @@
         var record = component.get("v.record");
         var subheading = '';
         for(var i=0; i<component.get("v.subHeadingFieldsAPI").length ;i++ ){
+            var fieldApi=component.get("v.subHeadingFieldsAPI")[i];
+            //Processing the relationship field
+            if(fieldApi.indexOf('.')>0){
+                var relatedRecord=record[fieldApi.split('.')[0]];
+                subheading = subheading + relatedRecord[fieldApi.split('.')[1]] + ' • ';
+            }
+            //sObject properties
+            else{
             if(record[component.get("v.subHeadingFieldsAPI")[i]]){
                 subheading = subheading + record[component.get("v.subHeadingFieldsAPI")[i]] + ' • ';
+            }
             }
         }
         subheading = subheading.substring(0,subheading.lastIndexOf('•'));
